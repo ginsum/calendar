4388bd199e3594fb44c3d9f15c62358c151781be
ginsum <immime12697@gmail.com>
2024-11-10 17:01:02 +0900

    CHORE: react-query 및 zustand 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import CalendarBody from "@/components/CalendarBody";
 import CalendarHeader from "@/components/CalendarHeader";
 import DutyFilterContainer from "@/components/DutyFilterContainer";
-import ModalContainer from "@/components/ModalContainer";
+import ReactQueryProvider from "@/query/Provider";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <DutyFilterContainer />
-      <CalendarHeader />
-      <CalendarBody />
-      <ModalContainer />
-    </div>
+    <ReactQueryProvider>
+      <div className="flex flex-col items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+        <DutyFilterContainer />
+        <CalendarHeader />
+        <CalendarBody />
+      </div>
+    </ReactQueryProvider>
   );
 }
